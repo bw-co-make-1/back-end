30f84d54f447ec9c066ae79aa33339dfa8f833c1
Jason Neale <jg13.1980@gmail.com>
2020-06-23 21:50:25 -0400

    reset master

--- index.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config()
-
-const server = require("./api/server")
-const port = process.env.PORT || 5000
-
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
--- routes/votes-router.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const Votes = require("../models/votes-model")
-
-
-router.get("/:postId", ( req, res ) => {
-    res.status(200).json({ message: "route is working." })
-})
-
-module.exports = router
