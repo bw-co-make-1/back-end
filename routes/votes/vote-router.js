d64474bbbbce4c0a7b4a11672be08e86de727924
Jason Neale <jg13.1980@gmail.com>
2020-06-25 14:53:01 -0400

    Added upvote end points and logic

--- routes/votes/vote-router.js
+++ routes/votes/vote-router.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
-const Users = require('./vote-mode.js')
+const Votes = require('./vote-model')
+const jwt = require("jsonwebtoken")
 
-router.get('/vote', async (req, res) => {
-   
+router.get('/:issueId', async (req, res) => {
+    await Votes.getVotesByIssue(req.params.issueId)
+    .then(votes => {
+      res.status(200).json(votes)
+    })
+    .catch(err => res.send(err))
+})
+
+router.post('/:issueId', async (req, res) => {
+  const user = jwt.decode(req.headers.authorization)
+
+  await Votes.postVotesByIssue(req.params.issueId, user.subject)
+  .then(votes => {
+    res.status(200).json(votes)
+  })
+  .catch(err => res.send(err))
 })
 
 module.exports = router
