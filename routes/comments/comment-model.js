16046f27155c4e9585ef647497f422908a02e12b
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-06-24 20:49:23 -0700

    Get all comments by issue and delet added

--- routes/comments/comment-model.js
+++ routes/comments/comment-model.js
@@ -1,7 +1,9 @@
 const db = require('../../config/dbConfig.js')
 module.exports = {
     getComments,
-    addComment
+    addComment,
+    byId,
+    removeComment
 }
 
 function getComments() {
@@ -12,3 +14,19 @@ function addComment(comment) {
     return db('comments')
     .insert(comment)
 }
+
+function byId(id){
+    return db('issues as I')
+    .join(' comments as C', 'C.issue_id','I.id'  )
+    .where('C.issue_id', '=', id)
+    .select('C.id',
+     'I.issue',
+     'C.comment'
+      )
+}
+
+function removeComment(id) {
+    return db("comments")
+      .where({ id })
+      .del();
+  }
--- routes/comments/comment-router.js
+++ routes/comments/comment-router.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const Comments = require('./comment-model.js')
 const jwtDecode = require('jwt-decode')
 
+
+//Post new issue with user information
 router.post('/:issue_id', async(req, res)=>{
     try {
         let decoded = jwtDecode(req.headers.authorization)
@@ -15,7 +17,22 @@ router.post('/:issue_id', async(req, res)=>{
         res.status(500).json({message: 'Error posting new Comment.'})
     }
 })
-
+//Get comments by Issue Id
+router.get('/:issue_id', async (req, res) => {
+    const { issue_id } = req.params
+    try {
+        const found = await Comments.byId(issue_id)
+        if (found.length > 0) {
+            res.status(200).json(found)
+        } else {
+            res.status(404).json('No Comment found.')
+        }
+    }
+    catch{
+        res.status(500).json('Error loading Comment.')
+    }
+})
+//Get all comments
 router.get('/', async(req, res)=>{
     try{
         const found = await Comments.getComments()
@@ -26,6 +43,20 @@ router.get('/', async(req, res)=>{
         }
     }catch{
         res.status(500).json({message: 'Error Loading Comment'})
+    }
+})
+//Delete comment using comment id
+router.delete('/:id', async(req, res)=>{
+    try{
+        const {id} = req.params
+        const deleted = await Comments.removeComment(id)
+        if (deleted){
+            res.json({message: `Comment ${id} deleted`})
+        }else{
+            res.json({message:'Unable to remove Comment.'})
+        }
+    }catch{
+        res.status(500).json({message: 'Error deleting Comment'})
     }
 })
 module.exports = router
