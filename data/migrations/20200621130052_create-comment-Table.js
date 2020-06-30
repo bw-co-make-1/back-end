b6bc829c7488ae2d4d0c67300c3679aef2f3d963
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-06-30 10:30:33 -0700

    migration

--- data/migrations/20200621130052_create-comment-Table.js
+++ data/migrations/20200621130052_create-comment-Table.js
@@ -1,25 +1,27 @@
 
 exports.up = async function(knex) {
-    await knex.schema.createTable("comments", comment => {
-      comment.increments();
-      comment.string("comment").notNullable();
-      comment.integer("user_id")
-      .notNullable()
-      .unsigned()
-      .references("id")
-      .inTable("users")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-      comment.integer("issue_id")
+  await knex.schema.createTable("comments", comments => {
+    comments.increments();
+    comments.string("comment").notNullable();
+    comments
+      .integer("issue_id")
       .notNullable()
       .unsigned()
       .references("id")
       .inTable("issues")
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
-      comment.timestamp("created_at").defaultTo(knex.fn.now());
-    });
-}
+    comments
+      .integer("user_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    comments.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
 exports.down = async function(knex) {
     await knex.schema.dropTableIfExists("comments");
 }
